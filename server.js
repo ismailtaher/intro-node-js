df606c060aa5599b79d1f22c98a6fd0724f168bd
ismailtaher <ismail.taher@ymail.com>
2025-06-17 18:49:59 +0500

    create your own module

--- server.js
+++ server.js
@@ -5,14 +5,18 @@
 // How NodeJS differs from Vanilla JS
 // 1) Node runs on server - not in a browser (backed not frontend)
 // 2) The console is the terminal window
-console.log("Hello World!");
+/* console.log("Hello World!"); */
 // 3) global object instead of window object
-console.log(global);
+/* console.log(global); */
 // 4) Has Common Core Modules that we will explore
 // 5) CommonJS modules instead of ES6 modules
 
 const os = require("os");
 const path = require("path");
+const { add, subtract } = require("./math");
+
+console.log(add(2, 3));
+console.log(subtract(2, 3));
 
 console.log("Type: " + os.type);
 console.log("Version: " + os.version);
